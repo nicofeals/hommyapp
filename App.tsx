04efee9e47be7812cb29e041faf58f3185304184
Nicolas Ceccarello <nico.ceccarello@gmail.com>
2020-03-11 17:33:28 +0100

    Add menu bar

--- App.tsx
+++ App.tsx
@@ -1,19 +1,91 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+import { LoadAssets, StyleGuide } from "./src/components";
+import Examples, { examples } from "./src/Examples";
+import AngularGradient from "./src/AngularGradient";
+import Accordion from "./src/Accordion";
+import ShaderAndMask from "./src/ShaderAndMask";
+import CircularProgress from "./src/CircularProgress";
+import Slider, { assets as sliderAssets } from "./src/Slider";
+import Tabbar from "./src/Tabbar";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const assets: number[] = [
+  ...examples.map(example => example.source),
+  ...sliderAssets
+];
+const fonts = {
+  "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  "SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+  "SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf")
+};
+
+const AppNavigator = createAppContainer(
+  createStackNavigator(
+    {
+      Tabbar: {
+        screen: Tabbar,
+        navigationOptions: {
+          title: "Tabbar",
+          headerBackTitle: null
+        }
+      },
+      Examples: {
+        screen: Examples,
+        navigationOptions: {
+          title: "The 5 min React Native",
+          headerBackTitle: null
+        }
+      },
+      Slider: {
+        screen: Slider,
+        navigationOptions: {
+          title: "Custom Slider",
+          headerBackTitle: null
+        }
+      },
+      CircularProgress: {
+        screen: CircularProgress,
+        navigationOptions: {
+          title: "Circular Progress"
+        }
+      },
+      AngularGradient: {
+        screen: AngularGradient,
+        navigationOptions: {
+          title: "Angular Gradient"
+        }
+      },
+      Accordion: {
+        screen: Accordion,
+        navigationOptions: {
+          title: "Accordion"
+        }
+      },
+      ShaderAndMask: {
+        screen: ShaderAndMask,
+        navigationOptions: {
+          title: "Shader And Mask"
+        }
+      }
+    },
+    {
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: StyleGuide.palette.primary,
+          borderBottomWidth: 0
+        },
+        headerTintColor: "white"
+      }
+    }
+  )
+);
+
+export default () => (
+  <LoadAssets {...{ assets, fonts }}>
+    <StatusBar barStyle="light-content" />
+    <AppNavigator />
+  </LoadAssets>
+);
